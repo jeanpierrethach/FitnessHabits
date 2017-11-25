3221194fd9a6661a2f6d023fc9d3f2498f6d7fa3
Andre Lachance <lachance.andre@ucs.inrs.ca>
2017-11-25 09:32:37 -0500

    Création des premières activitys/Fragments
    -- Manifest modifier pour mettre paramètres comme page de départ

---
+++ FitnessHabitsAndroid/app/src/main/java/com/strudelauxpommes/fitnesshabits/parameters/ProfileFragment.java
@@ -0,0 +1,22 @@
+package com.strudelauxpommes.fitnesshabits.parameters;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.strudelauxpommes.fitnesshabits.R;
+
+public class ProfileFragment extends Fragment {
+
+    public ProfileFragment() {
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
+}
